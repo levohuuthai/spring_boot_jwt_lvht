8acaaf9d135f540a0875ba131ee20189a3ab76ca
ThaiLe <levohuuthai1@gmail.com>
2021-09-22 14:09:53 +0700

    add .

---
+++ src/main/java/com/example/spring_boot_jwt_lvht/SpringBootJwtLvhtApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_jwt_lvht;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJwtLvhtApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJwtLvhtApplication.class, args);
+    }
+
+}
